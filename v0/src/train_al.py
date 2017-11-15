74fc49359d91521b1e247f6853cb18f776e849f0
Jason Ma <jaysunmah@gmail.com>
2017-11-15 00:20:41 -0500

    added good milestone to al project

--- v0/src/train_al.py
+++ v0/src/train_al.py
@@ -47,7 +47,11 @@ def get_image_list(data_dir):
     random.shuffle(images)
     return (images, classes)
 
-
+'''
+This function will present an image, and return
+some "hardcoded" value for what class we manually
+assign it. 
+'''
 def present_image(path):
     frame = cv2.imread(path)
     cv2.imshow('frame', frame)
@@ -55,26 +59,44 @@ def present_image(path):
         ch = cv2.waitKey()
         # this is hardcoded for SEA LION
         if ch & 0xFF == ord('a'):
-            return 'sealion'
+            return 'otter'
         # this is hardcoded for DARTH VADER
         if ch & 0xFF == ord('l'):
             return 'darthvader'
 
 '''
+Evaluates model in terms of performance with
+source of truth to validate
+'''
+def evaluate_model(model, data, src_of_truth):
+    preds = model.predict(data)
+    labels = np.array([int(x) for (f, x) in src_of_truth])
+    diffs = np.bitwise_xor(preds, labels)
+    accuracy = 100 - (sum(diffs) / len(diffs) * 100)
+    print("Model Accuracy:", str(accuracy) + "%")
+    return accuracy
+
+
+'''
 Centerpiece to our entire project. Should be very robust in the future,
 and have good sampling techniques
 '''
-def get_query_data(features, labels, i):
+def get_query(features, labels, batch_size):
     # TODO: implement better selection heuristic
-    feature_slice = features[i*10:i*10+10]
-    label_slice = labels[i*10:i*10+10]
+    feature_slice = features[:batch_size]
+    label_slice = labels[:batch_size]
 
     result = []
     for i, feature in enumerate(feature_slice):
         result.append((feature, labels[i][0]))
-    return result
 
-# we take in a massive list of images(shuffled), and we save their feature data
+    return (result, features[batch_size:], labels[batch_size:])
+
+'''
+we take in a massive list of images(shuffled), and we save their feature data
+Future improvement of this should be to display some progress bar, as this
+can take a long time to compute
+'''
 def create_bottleneck_features(images):
     model = VGG16(weights='imagenet', include_top=False)
     features = []
@@ -90,63 +112,92 @@ def create_bottleneck_features(images):
     np.save(join(os.getcwd(), "weights/al_features.npy"), np.array(features))
     np.save(join(os.getcwd(), "weights/al_labels.npy"), images)
 
-def train_classifier(data_dir):
-    # step 0. initialize our model
+def train_classifier(data_dir, reshuffle_data, iters, batch_size):
+    '''
+    step 0. initialize our model
+    '''
     clf = LinearSVC(multi_class="ovr", loss='hinge', verbose=True, max_iter=250, C=10**5)
-    # step 1. get list of all images
+
+    '''
+    step 1. get list of all images
+            will shuffle images, but classes order should remain stable
+    '''
     (images,classes) = get_image_list(data_dir)
 
-
-    # if True: #check if we are missing files OR we want to override them
-    #     print("Creating bottleneck features")
-    #     create_bottleneck_features(images)
+    '''
+    step 2. process / retrieve all(?) feature data, if applicable
+            after this step, we should have some list of unlabeled data,
+            and some list of source of truths which we can use to lookup
+            our "unlabeled" data when evaluating for accuracy
+    '''
+    
+    #check if we are missing files OR we want to override them
+    if not isfile(join(os.getcwd(), "weights/al_features.npy")) or reshuffle_data: 
+        print("Creating bottleneck features")
+        create_bottleneck_features(images)
 
     # bottleneck features is array of processed images
     bottleneck_features = np.load(join(os.getcwd(), "weights/al_features.npy"))
     # bottleneck labels is array of [image path, label] arrays
     bottleneck_labels = np.load(join(os.getcwd(), "weights/al_labels.npy"))
 
-    unlabeled_data = bottleneck_labels
+    '''
+    checkpoint 2.1: at this point, load up unlabeled data and src_of_truth lists.
+            unlabeled_data, can be "undefined", but src_of_truth MUST be
+            tuple of (img_path, class)
+    '''
+    unlabeled_data = bottleneck_features
+    src_of_truth = bottleneck_labels
 
-    # step 2. pick ten random images to initially manually label,
-    # put them into some list
-    X = []
-    y = []
-    for i in range(10):
+    '''
+    step 3: for ITER iterations, with BATCH images in each batch,
+            we query these image batches for manual labeling
+            we append these new data + labels to my_data and my_labels
+            which we will use to retrain our clf classifier (default=SVM)
+    '''
+    my_data = []
+    my_labels = []
+    accuracies = []
+    for i in range(iters):
         print("Querying on iteration:", i)
         # get_query_data will look through these features, and based
         # on "some" heuristic (random for now), it will return the
-        # processed features, its index, and image path
-        query = get_query_data(bottleneck_features, unlabeled_data, i)
+        # the query, as well as the new unlabeled dataset and src of truth
+        # query MUST have some image path zipped to end of tuple
+        (query,unlabeled_data,src_of_truth) = get_query(unlabeled_data, src_of_truth, batch_size)
         # manually label our current data
         for (features, img_path) in query:
             manual_label = classes[present_image(img_path)]
-            X.append(features)
-            y.append(manual_label)
+            my_data.append(features)
+            my_labels.append(manual_label)
 
-        print("Fitting model..")
-        clf.fit(X,y)
+        # Fit our classifier with our labeled data
+        clf.fit(np.array(my_data),np.array(my_labels))
 
-        #TODO: Evaluate periodic accuracy of our svm model with the rest of our
-        # unlabeled data!!
+        # Evaluate periodic accuracy of our svm model with the rest of our
+        # unlabeled data
+        acc = evaluate_model(clf, unlabeled_data, src_of_truth)
+        accuracies.append(acc)
 
+    #TODO plot our accuracies with respect to its index
+
+    '''
+    step 4: return / save our model!
+    '''
+
+    #TODO Not yet implemented
 
 if __name__=='__main__':
-    '''
-    First, we vectorize our images into nd arrays, along with their
-    corresponding labels
-    We store this as our source of truth
+    parser = argparse.ArgumentParser(description='Main script for developing active learning model')
+    parser.add_argument('--reshuffle_data', '-r', default=0, type=int,
+        help='Flag to reshuffle data and reprocess bottleneck features and labels')
+    parser.add_argument('--iterations', '-i', default=10, type=int,
+        help='How many iterations we want to run')
+    parser.add_argument('--batch_size', '-b', default=10, type=int,
+        help='How large we want our batch sizes to be')
 
-    We first randomly(?) pick 10 random vectors to label
+ 
+    args = parser.parse_args()
+    reshuffle_flag = args.reshuffle_data == 1
 
-    (*)Treat this as our current feature, label dataset.
-        -> train up our model using this X, y dataset. (Default=SVM)
-
-    With this new model, rerun on our entire dataset,
-        -> based on some heuristic (deafult=random), select top 10 features
-
-    Label these 10 features, append them to X' = X U {features}, Y' = Y U {labels}
-
-    Rinse and repeat until 100 labels (go back to step *)
-    '''
-    train_classifier(join(os.getcwd(), "data"))
+    train_classifier(join(os.getcwd(), "data"), reshuffle_flag, args.iterations, args.batch_size)
